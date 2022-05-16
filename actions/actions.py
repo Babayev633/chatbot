e2da4b8d9db6f9985ebe9fd5cb4f7c301038dcdd
Babayev633 <99037615+Babayev633@users.noreply.github.com>
2022-05-16 15:30:40 +0400

    Add files via upload

--- actions/actions.py
+++ actions/actions.py
@@ -1,33 +1,60 @@
+from asyncio.windows_events import NULL
 from asyncore import dispatcher
 from typing import Any, Text, Dict, List, Union
 
 
 from rasa_sdk import Action, Tracker
-from rasa_sdk.events import EventType
+from rasa_sdk.events import EventType, UserUtteranceReverted
 from rasa_sdk.executor import CollectingDispatcher
 
 from urllib import response
 import urllib.parse
 import urllib.request
 import webbrowser
+import requests
 import json
 import ast
+import pandas as pd
 
 import sqlite3
 path_to_db = "actions/store.db"
+
+class Time(Action):
+    def name(self) -> Text:
+        return "action_time"
+    
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> Dict[Text, Any]:
+        Curl_time_api = "http://worldtimeapi.org/api/timezone/Asia/Baku"
+        
+
+        response = requests.get(Curl_time_api)
+        data = json.loads(response.text)
+
+        try:   
+            print(data)
+            time = data['datetime']
+        
+            print(f"Exact time is: {time[11:19]}")
+            text = f"Exact time is: {time[11:19]}"
+            dispatcher.utter_message(text)  
+        except IndexError:
+            dispatcher.utter_message("Something went wrong with WorldTimeApi, please try again later")
+
+        return[]
 
 class ActionPPT(Action):
     def name(self) -> Text:
         return "action_ppt"
     
-    async def run(
-        self,
-        despatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> List[Dict[Text, Any]]:
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> Dict[Text, Any]:
+        #use this command if connected to external server or user-id based system (as server runs locally, command will open tab in admin pc)
+        #webbrowser.open(webpage_url)
         webpage_url="https://sdpecommerceproject.wixsite.com/ecommerceplatform"
-        webbrowser.open(webpage_url)
+        dispatcher.utter_template("utter_presentationppt", tracker, link = webpage_url)
         return[]
 
 
@@ -102,7 +129,44 @@ class ActionCategorySearch(Action):
         return[]
 
 
+class SearchProductName(Action):
+    def name(self) -> Text:
+        return "action_searchapi"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> Dict[Text, Any]:
+
+
+        pname = tracker.get_slot("pname")
+        api_address = "http://35.223.157.105:30006/api/product/search/"
+        url = api_address + pname
+
+        response = requests.get(url)
+        data = json.loads(response.text)
+        df = pd.DataFrame(data, columns = ['id', 'name','amount','cost','details','subcategoryId','storeId', 'photoUrl'])
+
+        try:
+            dispatcher.utter_message(text = 'Found these products: \n')
+            for i in range(len(df)) :
+                id = df.loc[i, "id"]
+                name = df.loc[i, "name"]
+                amount = df.loc[i, "amount"]
+                cost = df.loc[i, "cost"]
+                details = df.loc[i, "details"]
+                subcategoryId = df.loc[i, "subcategoryId"]
+                storeId = df.loc[i, "storeId"]
+                photoUrl = df.loc[i, "photoUrl"]
         
+                print(f"id: {id} \nname: {name}, \namount: {amount}, \ncost: {cost}₼, \ndetails: {details}, \nsubcategoryId: {subcategoryId}, \nstoreId: {storeId}, \nphotoUrl: {photoUrl} \n\n")
+                text1 = f"id: {id} \nname: {name}, \namount: {amount}, \ncost: {cost}₼, \ndetails: {details}, \nsubcategoryId: {subcategoryId}, \nstoreId: {storeId}, \nphotoUrl: {photoUrl}"
+                dispatcher.utter_message(text1)
+        except IndexError:
+            dispatcher.utter_message("Nothing found")
+
+            
+
+        return[]
 
 
 
